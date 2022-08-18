9801beef9cf488f12beda9a091a9b4061c65b94d
amine aqebli <aqebli36@gmail.com>
2022-08-18 22:38:23 +0100

    inputs two way binding

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from './AddUser.module.css';
 
+
 const AddUser = props => {
+    const [entredUsername, setEntredUsername] = useState('');
+    const [entredAge, setEntredAge] = useState('');
 
     const addUserHandler = (event) => {
         event.preventDefault();
+    };
+
+    const usernmaeChangeHandler = event => {
+        setEntredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = event => {
+        setEntredAge(event.target.value);
     };
 
     return (
@@ -14,9 +25,9 @@ const AddUser = props => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" onChange={usernmaeChangeHandler}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" onChange={ageChangeHandler}/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
